c8b312408b2132cc8b9f3b177b508aafa94866bd
BonniFO <bonnifo@gmail.com>
2023-12-07 19:59:45 +0300

    07.12.23

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args)
     {
-
+        int[] nums = new int[] {1,2,3,4};
+        System.out.println(nums[1]);
+        ArrayList<Integer> = new ArrayList<Integer>();
     }
 }
